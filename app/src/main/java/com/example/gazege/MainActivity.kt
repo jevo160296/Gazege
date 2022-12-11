312ad469ff5b40aa15c93a174ea865fe9ecf9fc4
Jose Miguel Millan <jevo160296@gmail.com>
2022-12-10 22:12:47 -0500

    Implementación de lista de personas con un método para añadir personas al pulsar un botón.

--- app/src/main/java/com/example/gazege/MainActivity.kt
+++ app/src/main/java/com/example/gazege/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.gazege
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.gazege.core.entities.Person
 import com.example.gazege.ui.theme.GazegeTheme
+import com.example.gazege.ui.views.PersonRecyclerView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
             GazegeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Page()
                 }
             }
         }
@@ -27,14 +31,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Page() {
+    var personList by remember {
+        mutableStateOf(
+            listOf(
+            Person(name = "Persona1"),
+            Person(name = "Persona2")
+        ))
+    }
+    Column {
+        PersonRecyclerView(personList = personList)
+        Button(
+            onClick = {
+                personList = listOf(
+                    *personList.toTypedArray(),
+                    Person(name = "Added Persona")
+                )
+             }) {
+            Text("Add")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GazegeTheme {
-        Greeting("Android")
+        Page()
     }
 }
