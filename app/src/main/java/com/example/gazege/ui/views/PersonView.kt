27480732df660d65d34cec7fb5f72c28212b1a43
Jose Miguel Millan <jevo160296@gmail.com>
2022-12-10 22:12:16 -0500

    Se implementa el RecyclerView para lista de personas.

---
+++ app/src/main/java/com/example/gazege/ui/views/PersonView.kt
@@ -0,0 +1,41 @@
+package com.example.gazege.ui.views
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gazege.core.entities.Person
+import com.example.gazege.ui.widgets.RecyclerView
+
+@Composable
+private fun PersonViewHolder(person: Person) {
+    Text(text = person.name)
+}
+
+@Composable
+fun PersonRecyclerView(personList: List<Person>) {
+    RecyclerView(elements = personList) {
+        PersonViewHolder(person = it)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewPersonItem() {
+    val person = Person(name = "Persona")
+    PersonViewHolder(person = person)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewPersonList() {
+    val personList = listOf(
+        Person(name = "Persona1"),
+        Person(name = "Persona2"),
+        Person(name = "Persona3"),
+        Person(name = "Persona4")
+    )
+    RecyclerView(
+        elements = personList,
+        viewHolder = { person -> PersonViewHolder(person = person) }
+    )
+}
