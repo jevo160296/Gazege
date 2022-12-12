86ac24978998ac911582c13e7d43e45643900e40
Jose Miguel Millan <jevo160296@gmail.com>
2022-12-11 21:51:35 -0500

    Material 3 implementation

--- app/src/main/java/com/example/gazege/MainActivity.kt
+++ app/src/main/java/com/example/gazege/MainActivity.kt
@@ -7,16 +7,22 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.FabPosition
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gazege.core.AppDatabase
 import com.example.gazege.core.AppRepository
 import com.example.gazege.core.entities.*
 import com.example.gazege.ui.theme.GazegeTheme
+import com.example.gazege.ui.theme.Shapes
+import com.example.gazege.ui.theme.Typography
 import com.example.gazege.ui.views.AccountRecyclerView
 import com.example.gazege.ui.views.PersonRecyclerView
 import com.example.gazege.ui.views.TransactionRecyclerView
@@ -47,7 +53,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.surface
                 ) {
                     Page(
                         personList,
@@ -84,68 +90,71 @@ fun Page(
 ) {
     Scaffold(
         floatingActionButton = {
-            Button(
+            FloatingActionButton(
                 onClick = {
-                    val random = Random.nextInt(3)
-                    val personAdd = random == 0
-                    val transAdd = random == 1
-                    val cuentaAdd = random == 2
-                    val maxPersonasId = personList.maxOfOrNull { it.id ?: -1 } ?: -1
-                    val maxAccountsId = accountList.maxOfOrNull { it.account.id ?: -1 } ?: -1
-                    val maxTransactionsId =
-                        transactionList.maxOfOrNull { it.transaction.id ?: -1 } ?: -1
-                    if (personAdd) {
-                        addPerson(Person(name = "Persona ${maxPersonasId + 1}"))
-                    } else if (cuentaAdd && personList.isNotEmpty()) {
-                        val cantPersonas = personList.size
-                        val selectedPerson = Random.nextInt(cantPersonas)
-                        addAccount(
-                            Account(
-                                name = "Account ${maxAccountsId + 1}",
-                                initial_balance = 1.0,
-                                ownerId = personList[selectedPerson].id ?: -1
-                            )
+                val random = Random.nextInt(3)
+                val personAdd = random == 0
+                val transAdd = random == 1
+                val cuentaAdd = random == 2
+                val maxPersonasId = personList.maxOfOrNull { it.id ?: -1 } ?: -1
+                val maxAccountsId = accountList.maxOfOrNull { it.account.id ?: -1 } ?: -1
+                val maxTransactionsId =
+                    transactionList.maxOfOrNull { it.transaction.id ?: -1 } ?: -1
+                if (personAdd) {
+                    addPerson(Person(name = "Persona ${maxPersonasId + 1}"))
+                } else if (cuentaAdd && personList.isNotEmpty()) {
+                    val cantPersonas = personList.size
+                    val selectedPerson = Random.nextInt(cantPersonas)
+                    addAccount(
+                        Account(
+                            name = "Account ${maxAccountsId + 1}",
+                            initial_balance = 1.0,
+                            ownerId = personList[selectedPerson].id ?: -1
                         )
-                    } else if (transAdd && accountList.size >= 2) {
-                        val cantAccounts = accountList.size
-                        val selectedSourceAccount = Random.nextInt(cantAccounts)
-                        val selectedDestinationAccount = Random.nextInt(cantAccounts)
-                        addTransaction(
-                            Transaction(
-                                amount = 0.0,
-                                description = "Trans ${maxTransactionsId + 1}",
-                                sourceId = accountList[selectedSourceAccount].account.id ?: -1,
-                                destinationId = accountList[selectedDestinationAccount].account.id
-                                    ?: -1,
-                                date = Date()
-                            )
+                    )
+                } else if (transAdd && accountList.size >= 2) {
+                    val cantAccounts = accountList.size
+                    val selectedSourceAccount = Random.nextInt(cantAccounts)
+                    val selectedDestinationAccount = Random.nextInt(cantAccounts)
+                    addTransaction(
+                        Transaction(
+                            amount = 0.0,
+                            description = "Trans ${maxTransactionsId + 1}",
+                            sourceId = accountList[selectedSourceAccount].account.id ?: -1,
+                            destinationId = accountList[selectedDestinationAccount].account.id
+                                ?: -1,
+                            date = Date()
                         )
-                    }
-                }) {
-                Text("Add")
+                    )
+                }
+            },
+                shape = Shapes.small
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_baseline_add_24),
+                    contentDescription = "Add"
+                )
             }
         },
+        floatingActionButtonPosition = FabPosition.End,
+        isFloatingActionButtonDocked = true,
         bottomBar = {
-            Button(onClick = {
-                if (personList.isNotEmpty()) {
-                    delPerson(personList[0])
-                }
-            }) {
-                Text(text = "Delete")
+            BottomAppBar {
+                Text(text = "Gazege", modifier = Modifier.padding(start=12.dp))
             }
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
             Column(modifier = Modifier.weight(1F)) {
-                Text("Personas")
+                Text("Personas", style = Typography.headlineMedium)
                 PersonRecyclerView(personList = personList)
             }
             Column(modifier = Modifier.weight(1F)) {
-                Text("Cuentas")
+                Text("Cuentas", style = Typography.headlineMedium)
                 AccountRecyclerView(accountList = accountList)
             }
             Column(modifier = Modifier.weight(1F)) {
-                Text("Transacciones")
+                Text("Transacciones", style = Typography.headlineMedium)
                 TransactionRecyclerView(transactionList = transactionList)
             }
         }
