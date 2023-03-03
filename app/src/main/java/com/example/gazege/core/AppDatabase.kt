5bbab47c75a1a4981b4dffa726edbc093d6c5771
Jose Miguel Millan <jevo160296@gmail.com>
2023-03-02 22:40:12 -0500

    Crear migracion de 3 a 4

--- app/src/main/java/com/example/gazege/core/AppDatabase.kt
+++ app/src/main/java/com/example/gazege/core/AppDatabase.kt
@@ -8,6 +8,7 @@ import com.example.gazege.core.dao.TransactionDao
 import com.example.gazege.core.entities.Account
 import com.example.gazege.core.entities.Person
 import com.example.gazege.core.entities.Transaction
+import com.example.gazege.core.migrations.Migrate34
 
 @Database(
     entities = [
@@ -16,8 +17,7 @@ import com.example.gazege.core.entities.Transaction
         Transaction::class],
     version = 4,
     autoMigrations = [
-        AutoMigration(from = 2, to = 3),
-        AutoMigration(from = 3, to = 4)
+        AutoMigration(from = 2, to = 3)
     ]
 )
 @TypeConverters(DateConverter::class)
@@ -33,6 +33,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance =
                     Room.databaseBuilder(context, AppDatabase::class.java, "app_database")
                         .fallbackToDestructiveMigrationFrom(1)
+                        .addMigrations(Migrate34())
                         .build()
                 INSTANCE = instance
                 instance
