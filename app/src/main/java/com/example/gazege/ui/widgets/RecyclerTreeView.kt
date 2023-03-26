9706f7ddc677d0989f86a31ceef2a4e1da22d9ac
Jose Miguel Millan <jevo160296@gmail.com>
2023-03-26 18:30:01 -0500

    Implementar item holder padding.

--- app/src/main/java/com/example/gazege/ui/widgets/RecyclerTreeView.kt
+++ app/src/main/java/com/example/gazege/ui/widgets/RecyclerTreeView.kt
@@ -1,6 +1,14 @@
 package com.example.gazege.ui.widgets
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
@@ -10,6 +18,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 
 interface Node<N, C : Node<N, C>> {
     val content: N
@@ -23,9 +34,11 @@ fun <N, C : Node<N, C>> RecyclerTreeView(
     groupSelector: (C) -> String? = { null },
     groupViewHolder: @Composable (String) -> Unit = { Text(it) },
     state: LazyListState = rememberLazyListState(),
+    itemHolderPaddingValues: PaddingValues = PaddingValues(),
     viewHolder: @Composable (C, TreeScope<N, C>) -> Unit
 ) {
     val expandedItems = remember { mutableStateListOf<C>() }
+    val layoutDirection = LocalLayoutDirection.current
     val treeScope = remember {
         mutableStateOf(
             TreeScope(
@@ -41,17 +54,27 @@ fun <N, C : Node<N, C>> RecyclerTreeView(
                     expandedItems.contains(it)
                 },
                 groupSelector = groupSelector,
-                groupViewHolder = groupViewHolder
+                groupViewHolder = groupViewHolder,
+                itemHolderPaddingValues = itemHolderPaddingValues,
+                layoutDirection = layoutDirection
             )
         )
     }
     LazyColumn(
         state = state
     ) {
+        val calculatedTop = itemHolderPaddingValues.calculateTopPadding()
+        val calculatedBottom = itemHolderPaddingValues.calculateBottomPadding()
+        item {
+            Spacer(modifier = Modifier.height(calculatedTop))
+        }
         nodes(
             nodes,
             treeScope = treeScope.value
         )
+        item {
+            Spacer(Modifier.height(calculatedBottom))
+        }
     }
 }
 
@@ -81,13 +104,21 @@ fun <N, C : Node<N, C>> LazyListScope.node(
     currentGroup: String?,
     treeScope: TreeScope<N, C>
 ) {
+    val itemHolderPaddingValues = treeScope.itemHolderPaddingValues
+    val layoutDirection = treeScope.layoutDirection
     if (currentGroup != null && previousGroup != currentGroup) {
         stickyHeader {
             treeScope.groupViewHolder(currentGroup)
         }
     }
     item {
-        treeScope.viewHolder(node, treeScope)
+        Row {
+            Spacer(Modifier.width(itemHolderPaddingValues.calculateStartPadding(layoutDirection)))
+            Box(Modifier.weight(1f)) {
+                treeScope.viewHolder(node, treeScope)
+            }
+            Spacer(Modifier.width(itemHolderPaddingValues.calculateEndPadding(layoutDirection)))
+        }
     }
     if (treeScope.isExpanded(node)) {
         nodes(
@@ -102,6 +133,8 @@ data class TreeScope<N, C : Node<N, C>>(
     val viewHolder: @Composable (C, TreeScope<N, C>) -> Unit,
     val groupViewHolder: @Composable (String) -> Unit,
     val groupSelector: (C) -> String?,
+    val itemHolderPaddingValues: PaddingValues,
+    val layoutDirection: LayoutDirection,
     val isExpanded: (C) -> Boolean,
     val toggleExpanded: (C) -> Unit
 )
