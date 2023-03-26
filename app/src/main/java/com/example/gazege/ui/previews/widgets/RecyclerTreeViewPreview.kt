6495ef91fbd778d3e647746f7b7de4c25ff329ed
Jose Miguel Millan <jevo160296@gmail.com>
2023-03-26 14:22:16 -0500

    Crear visualización de prueba para recycler tree view.

---
+++ app/src/main/java/com/example/gazege/ui/previews/widgets/RecyclerTreeViewPreview.kt
@@ -0,0 +1,54 @@
+package com.example.gazege.ui.previews.widgets
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gazege.ui.theme.GazegeTheme
+import com.example.gazege.ui.widgets.Node
+import com.example.gazege.ui.widgets.RecyclerTreeView
+
+data class CuentaEHijos(
+    val nombre: String,
+    val valor: Int,
+    val hijos: List<CuentaEHijos>
+)
+
+data class CuentaNode(
+    val item: CuentaEHijos
+) : Node<CuentaNode> {
+    override val content: String
+        get() = item.nombre
+    override val children: List<CuentaNode>
+        get() = item.hijos.map { CuentaNode(it) }
+}
+
+@Preview(showBackground = true, heightDp = 400, widthDp = 200)
+@Composable
+fun RecyclerViewPreview() {
+    val cuentas = listOf(
+        CuentaEHijos("Efectivo", 10000, listOf()),
+        CuentaEHijos(
+            "Banco", 20000, listOf(
+                CuentaEHijos("Banco 1", 5000, listOf()),
+                CuentaEHijos("Banco 2", 6000, listOf())
+            )
+        )
+    )
+    GazegeTheme {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+        ) {
+            RecyclerTreeView(
+                cuentas.map {
+                    CuentaNode(it)
+                }
+            )
+        }
+    }
+}
