6fa2236f6798de41c058c3e6e30d2d7929eaeddd
Jose Miguel Millan <jevo160296@gmail.com>
2023-03-27 14:46:45 -0500

    Agregar padding a los headers.

--- app/src/main/java/com/example/gazege/ui/widgets/treeview/RecyclerTreeView.kt
+++ app/src/main/java/com/example/gazege/ui/widgets/treeview/RecyclerTreeView.kt
@@ -90,18 +90,26 @@ private fun <N, C : Node<N, C>> LazyListScope.node(
 ) {
     val itemHolderPaddingValues = treeScope.itemHolderPaddingValues
     val layoutDirection = treeScope.layoutDirection
+    val startPadding = itemHolderPaddingValues.calculateStartPadding(layoutDirection)
+    val endPadding = itemHolderPaddingValues.calculateEndPadding(layoutDirection)
     if (currentGroup != null && previousGroup != currentGroup) {
         stickyHeader {
-            treeScope.groupViewHolder(currentGroup)
+            Row {
+                Spacer(Modifier.width(startPadding))
+                Box(Modifier.weight(1f)) {
+                    treeScope.groupViewHolder(currentGroup)
+                }
+                Spacer(Modifier.width(endPadding))
+            }
         }
     }
     item {
         Row {
-            Spacer(Modifier.width(itemHolderPaddingValues.calculateStartPadding(layoutDirection)))
+            Spacer(Modifier.width(startPadding))
             Box(Modifier.weight(1f)) {
                 treeScope.viewHolder(node, treeScope)
             }
-            Spacer(Modifier.width(itemHolderPaddingValues.calculateEndPadding(layoutDirection)))
+            Spacer(Modifier.width(endPadding))
         }
     }
     if (treeScope.isExpanded(node)) {
