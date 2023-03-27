2a8b59d550e84066bf28749261b4052d32f85798
Jose Miguel Millan <jevo160296@gmail.com>
2023-03-27 00:08:46 -0500

    Implementar DropDownTreeMenu

--- app/src/main/java/com/example/gazege/ui/widgets/DropDownMenu.kt
+++ app/src/main/java/com/example/gazege/ui/widgets/DropDownMenu.kt
@@ -1,14 +1,25 @@
 package com.example.gazege.ui.widgets
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
+import com.example.gazege.ui.widgets.treeview.ColumnTreeView
+import com.example.gazege.ui.widgets.treeview.Node
+import com.example.gazege.ui.widgets.treeview.NodeId
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -74,6 +85,95 @@ fun <T> DropDownMenu(
                 onItemClick = onItemClick,
                 itemToString = itemToString,
                 onExpandedChange = onExpandedChange
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun <N, C : Node<N, C>> DropDownTreeMenu(
+    dropDownExpanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit,
+    options: List<C>,
+    selectedItem: C?,
+    itemToString: (C?) -> String,
+    onItemClick: (C) -> Unit,
+    label: @Composable () -> Unit,
+    groupByKeySelector: ((C) -> String)? = null
+) {
+    val groupedOptions = options.groupBy { groupByKeySelector?.invoke(it) }
+    ExposedDropdownMenuBox(
+        expanded = dropDownExpanded,
+        onExpandedChange = onExpandedChange
+    ) {
+        TextField(
+            modifier = Modifier.menuAnchor(),
+            value = itemToString(selectedItem),
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = dropDownExpanded)
+            },
+            label = label,
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        ExposedDropdownMenu(
+            expanded = dropDownExpanded,
+            onDismissRequest = { onExpandedChange(false) }
+        ) {
+            OptionsGroupTreeView(
+                groupedOptions = groupedOptions,
+                onItemClick = onItemClick,
+                itemToString = itemToString,
+                onExpandedChange = onExpandedChange
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun <N, C : Node<N, C>> OptionsGroupTreeView(
+    groupedOptions: Map<String?, List<C>>,
+    onItemClick: (C) -> Unit,
+    itemToString: (C?) -> String,
+    onExpandedChange: (Boolean) -> Unit
+) {
+    val nodes: List<C> = groupedOptions.flatMap {
+        it.value
+    }
+    val inverseMap: Map<NodeId, String?> = groupedOptions.flatMap { (key, value) ->
+        value.map {
+            it.id() to key
+        }
+    }.toMap()
+    ColumnTreeView(nodes = nodes, groupSelector = {
+        inverseMap[it.id()]
+    }) { node, treeScope ->
+        Row {
+            Spacer(modifier = Modifier.width(node.level.dp * 8))
+            if (node.children.isNotEmpty()) {
+                IconToggleButton(
+                    modifier = Modifier.width(32.dp),
+                    checked = treeScope.isExpanded(node),
+                    onCheckedChange = { treeScope.toggleExpanded(node) }
+                ) {
+                    Icon(
+                        painter = rememberVectorPainter(image = Icons.Default.KeyboardArrowRight),
+                        contentDescription = "Desc"
+                    )
+                }
+            } else {
+                Spacer(Modifier.width(32.dp))
+            }
+            DropdownMenuItem(
+                text = { Text(itemToString(node)) },
+                onClick = {
+                    onExpandedChange(false)
+                    onItemClick(node)
+                },
+                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
             )
         }
     }
