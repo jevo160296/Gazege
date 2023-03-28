6d2b2fcbfaa951689287b29bfa86f1dd54e713c7
Jose Miguel Millan <jevo160296@gmail.com>
2023-03-28 18:11:03 -0500

    Corregir error al escribir números negativos.

--- app/src/main/java/com/example/gazege/ui/widgets/TextField.kt
+++ app/src/main/java/com/example/gazege/ui/widgets/TextField.kt
@@ -143,17 +143,27 @@ class NumberTransformation(
     }
 
     class Offset(
-        originalNumberLength: Int, val decimalPointPosition: Int? = null
+        originalNumberLength: Int, val decimalPointPosition: Int? = null, startsWithMinus: Boolean
     ) : OffsetMapping {
         private val wholePartOriginalIntegerLength: Int =
             originalNumberLength - if (decimalPointPosition != null) {
                 3
             } else {
                 0
-            }
+            } -
+                    if (startsWithMinus) {
+                        1
+                    } else {
+                        0
+                    }
 
         private val transformedIntegerLength =
-            wholePartOriginalIntegerLength + calculateTotalThousandSeparatorCount()
+            wholePartOriginalIntegerLength + calculateTotalThousandSeparatorCount() +
+                    if (startsWithMinus) {
+                        1
+                    } else {
+                        0
+                    }
 
         override fun originalToTransformed(offset: Int): Int =
             offset + calculateLeftTotalThousandSeparatorCount(offset)
@@ -195,7 +205,7 @@ class NumberTransformation(
             FormatType.Double -> text.indexOf(".") + 1
             FormatType.Int -> null
         }
-        val offsetMapping = Offset(text.length, decimalPointPosition)
+        val offsetMapping = Offset(text.length, decimalPointPosition, text.startsWith('-'))
         return TransformedText(
             text = AnnotatedString(text = textFormatted), offsetMapping = offsetMapping
         )
@@ -235,10 +245,6 @@ class NumberTransformation(
 @Preview(showBackground = true, heightDp = 620, widthDp = 420)
 @Composable
 private fun TextFieldPreview() {
-    val numberTransformation = NumberTransformation.Offset(6)
-    (0..6).map {
-        numberTransformation.originalToTransformed(it)
-    }
     GazegeTheme {
         var number by remember {
             mutableStateOf(BigDecimal(0))
