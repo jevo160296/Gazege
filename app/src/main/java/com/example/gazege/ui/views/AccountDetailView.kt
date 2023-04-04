9011976803d0f868dabee57f478871791e8c1b25
Jose Miguel Millan <jevo160296@gmail.com>
2023-04-04 00:40:50 -0500

    Agregar graficos al AccountDetailView.kt

--- app/src/main/java/com/example/gazege/ui/views/AccountDetailView.kt
+++ app/src/main/java/com/example/gazege/ui/views/AccountDetailView.kt
@@ -1,24 +1,21 @@
 package com.example.gazege.ui.views
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
-import androidx.compose.material.Text
 import androidx.compose.material3.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gazege.R
 import com.example.gazege.core.dao.AccountDao
 import com.example.gazege.core.entities.*
+import com.example.gazege.core.firstDayOfMonth
 import com.example.gazege.ui.accountDeleitionConfirmationBuilder
 import com.example.gazege.ui.doubleToString
 import com.example.gazege.ui.theme.GazegeTheme
@@ -26,8 +23,75 @@ import com.example.gazege.ui.transactionDeleitionConfirmationBuilder
 import com.example.gazege.ui.widgets.DataView
 import com.example.gazege.ui.widgets.LargeEmphasis
 import com.example.gazege.ui.widgets.MediumHeadline
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.entry.ChartEntry
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import java.time.Period
+
+class Entry(
+    val date: LocalDate,
+    override val x: Float,
+    override val y: Float
+) : ChartEntry {
+    override fun withY(y: Float): ChartEntry = Entry(date, x, y)
+}
+
+@Composable
+fun Plot(transactions: List<Transaction>) {
+    val maxDate = transactions.maxOfOrNull { it.date }
+    val minDate = transactions.minOfOrNull { it.date }
+    val dateRange = if (maxDate != null && minDate != null) {
+        Period.between(minDate, maxDate).days
+    } else {
+        null
+    }
+    val groupedTransactions = transactions
+        .sortedBy { it.date }
+        .groupBy {
+            if (dateRange != null && dateRange > 60) {
+                firstDayOfMonth(it.date)
+            } else {
+                it.date
+            }
+        }
+        .map {
+            it.key to it.value.sumOf { trx -> trx.amount }
+        }
+        .mapIndexed { index, (date, y) ->
+            Entry(date, index.toFloat(), y.toFloat())
+        }
+    val chartEntryModel = ChartEntryModelProducer(groupedTransactions).getModel()
+    val horizontalAxisValueFormatter =
+        AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+            (chartValues.chartEntryModel.entries.first().getOrNull(value.toInt()) as? Entry)
+                ?.date
+                ?.run { "$dayOfMonth/$monthValue" }
+                .orEmpty()
+        }
+    val verticalAxisValueFormatter = AxisValueFormatter<AxisPosition.Vertical.Start> { value, _ ->
+        doubleToString(value.toDouble())
+    }
+    Box(
+        Modifier
+            .height(130.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Chart(
+            chart = lineChart(),
+            model = chartEntryModel,
+            bottomAxis = bottomAxis(valueFormatter = horizontalAxisValueFormatter),
+            startAxis = startAxis(valueFormatter = verticalAxisValueFormatter)
+        )
+    }
+}
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -95,21 +159,14 @@ fun AccountDetail(
                 enabled = false
             )
         }
-        Box(
-            Modifier
-                .border(BorderStroke(1.dp, Color.Blue))
-                .height(120.dp)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Plot")
-        }
-        MediumHeadline(text = stringResource(id = R.string.transacciones))
         val transactions = listOf(
             *account.accountAndOwnerWithTransactions.inTransactions.toTypedArray(),
             *account.accountAndOwnerWithTransactions.outTransactions.toTypedArray()
         )
             .sortedByDescending { it.date }
+            .filter { it.date >= startDate && it.date <= endDate }
+        Plot(transactions)
+        MediumHeadline(text = stringResource(id = R.string.transacciones))
         val transactionsAndAccountsAndCategory = TransactionAndAccountsAndCategory.from(
             transactions,
             allAccounts,
