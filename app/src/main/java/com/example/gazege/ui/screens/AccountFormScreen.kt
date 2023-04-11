3a1048da7470e2050ce9900cd321eea7fc2ac143
Jose Miguel Millan <jevo160296@gmail.com>
2023-04-10 21:51:09 -0500

    Refactor split EntityFormScreen.kt into three separate files.

---
+++ app/src/main/java/com/example/gazege/ui/screens/AccountFormScreen.kt
@@ -0,0 +1,103 @@
+package com.example.gazege.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.gazege.core.entities.Account
+import com.example.gazege.core.entities.AccountAndOwner
+import com.example.gazege.core.entities.Person
+import com.example.gazege.ui.savers.PartialAccount
+import com.example.gazege.ui.savers.PartialAccountAndOwner
+import com.example.gazege.ui.savers.accountAndOwnerSaver
+import com.example.gazege.ui.views.account.AccountAndOwnerForm
+import com.example.gazege.ui.widgets.Form
+import com.example.gazege.ui.widgets.toSignedBigDecimal
+
+@Composable
+fun AccountFormScreen(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(),
+    itemSpacing: Dp = 0.dp,
+    accountAndOwner: AccountAndOwner? = null,
+    accountAndOwnerList: List<AccountAndOwner>,
+    personList: List<Person>,
+    currentBalance: Double,
+    incomeAccount: Account?,
+    outcomeAccount: Account?,
+    onSetIncomeOutcomeAccount: () -> Unit,
+    onPersonAddRequested: () -> Unit,
+    onAccountAndOwnerAdd: (Account, Double, SnackbarHostState, Int?, Int?) -> Unit
+) {
+    var currentBalanceState by rememberSaveable {
+        mutableStateOf(currentBalance.toSignedBigDecimal())
+    }
+    var accountAndOwnerState by rememberSaveable(
+        stateSaver = accountAndOwnerSaver
+    ) {
+        mutableStateOf(
+            if (accountAndOwner != null) {
+                PartialAccountAndOwner(
+                    account = accountAndOwner.account.let {
+                        PartialAccount(
+                            id = it.id,
+                            name = it.name,
+                            ownerId = it.ownerId,
+                            includedInTotal = it.includedInTotal,
+                            isIncome = it.isIncome,
+                            isOutcome = it.isOutcome,
+                            parentId = it.parentId
+                        )
+                    },
+                    owner = accountAndOwner.owner
+                )
+            } else {
+                PartialAccountAndOwner.blankEntity()
+            }
+        )
+    }
+    val completeState = accountAndOwnerState.isComplete()
+    val snackbarHostState = SnackbarHostState()
+    val saveAccount = {
+        val fullAccountAndOwner = accountAndOwnerState.toFull()
+        onAccountAndOwnerAdd(
+            fullAccountAndOwner.account,
+            currentBalanceState.toDouble(),
+            snackbarHostState,
+            incomeAccount?.id,
+            outcomeAccount?.id
+        )
+    }
+    Form(
+        modifier = modifier,
+        onSaveClicked = saveAccount,
+        isSavedButtonEnabled = completeState,
+        snackbarHostState = snackbarHostState,
+        title = "Account"
+    ) {
+        AccountAndOwnerForm(
+            contentPadding = contentPadding,
+            itemSpacing = itemSpacing,
+            accountAndOwner = accountAndOwnerState,
+            accountAndOwnerList = accountAndOwnerList,
+            personList = personList,
+            currentBalance = currentBalanceState,
+            onCurrentBalanceChanged = { currentBalanceState = it },
+            onPersonAddRequested = onPersonAddRequested,
+            onAccountAndOwnerChanged = {
+                accountAndOwnerState = it
+            },
+            incomeAccount = incomeAccount,
+            outcomeAccount = outcomeAccount,
+            onSetIncomeOutcomeAccount = onSetIncomeOutcomeAccount,
+            onDoneAction = saveAccount,
+            isComplete = completeState
+        )
+    }
+}
