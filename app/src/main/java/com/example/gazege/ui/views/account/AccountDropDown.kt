822327f71ac5cfd39b9f9454c04dbed26c8d6e0b
Jose Miguel Millan <jevo160296@gmail.com>
2023-04-19 17:02:05 -0500

    Implementar ComboBox
    El ComboBox permite escribir para filtrar los resultados mostrados.

--- app/src/main/java/com/example/gazege/ui/views/account/AccountDropDown.kt
+++ app/src/main/java/com/example/gazege/ui/views/account/AccountDropDown.kt
@@ -1,22 +1,16 @@
 package com.example.gazege.ui.views.account
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.unit.dp
 import com.example.gazege.core.entities.AccountAndOwner
 import com.example.gazege.ui.views.transaction.AccountAndOwnerNode
-import com.example.gazege.ui.widgets.DefaultDropDownViewHolder
-import com.example.gazege.ui.widgets.DropDownTreeMenu
+import com.example.gazege.ui.widgets.TreeComboBox
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,7 +43,7 @@ fun AccountDropDownMenu(
         mutableStateOf(false)
     }
     val itemToString = { it: AccountAndOwnerNode? -> it?.content?.account?.name ?: "" }
-    DropDownTreeMenu(
+    TreeComboBox(
         dropDownExpanded = dropDownExpanded,
         onExpandedChange = {
             dropDownExpanded = !dropDownExpanded
@@ -58,33 +52,12 @@ fun AccountDropDownMenu(
         selectedItem = selectedAccountNode,
         itemToString = itemToString,
         label = label,
-        viewHolder = { node ->
-            DefaultDropDownViewHolder(
-                itemToString = itemToString,
-                node = node,
-                onExpandedChange = {
-                    dropDownExpanded = !dropDownExpanded
-                },
-                onItemClick = {
-                    dropDownExpanded = false
-                    onItemClick(it)
-                },
-                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding.let {
-                    val layoutDirection = LocalLayoutDirection.current
-                    PaddingValues(
-                        start = 8.dp,
-                        top = it.calculateTopPadding(),
-                        bottom = it.calculateBottomPadding(),
-                        end = it.calculateEndPadding(layoutDirection)
-                    )
-                },
-                enabled = node.isActive
-            )
-        },
+        onItemClick = onItemClick,
         canClearSelection = canClearSelection,
         onClearSelectionClicked = onClearSelectionClicked,
         keyboardActions = keyboardActions,
-        keyboardOptions = keyboardOptions
+        keyboardOptions = keyboardOptions,
+        nodeEnabled = { it.isActive }
     ) {
         it.content.owner.name
     }
