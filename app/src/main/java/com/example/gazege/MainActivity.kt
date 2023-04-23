258f6c0dd2b903df47481c88e04ec2de087f8f80
Jose Miguel Millan <jevo160296@gmail.com>
2023-04-23 12:09:42 -0500

    Refactor: Split MainNavHost

--- app/src/main/java/com/example/gazege/MainActivity.kt
+++ app/src/main/java/com/example/gazege/MainActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.zIndex
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.gazege.core.AppDatabase
 import com.example.gazege.core.AppRepository
@@ -35,42 +34,7 @@ import com.example.gazege.ui.Settings
 import com.example.gazege.ui.fragments.IconVisibility
 import com.example.gazege.ui.fragments.SplashScreenFragment
 import com.example.gazege.ui.fragments.TextVisibility
-import com.example.gazege.ui.navigation.navigateToAccountDetail
-import com.example.gazege.ui.navigation.navigateToAddAccount
-import com.example.gazege.ui.navigation.navigateToAddCategory
-import com.example.gazege.ui.navigation.navigateToAddOneBudget
-import com.example.gazege.ui.navigation.navigateToAddPerson
-import com.example.gazege.ui.navigation.navigateToAddTransaction
-import com.example.gazege.ui.navigation.navigateToEditAccount
-import com.example.gazege.ui.navigation.navigateToEditBudget
-import com.example.gazege.ui.navigation.navigateToEditCategory
-import com.example.gazege.ui.navigation.navigateToEditOneBudget
-import com.example.gazege.ui.navigation.navigateToEditPerson
-import com.example.gazege.ui.navigation.navigateToEditTransaction
-import com.example.gazege.ui.navigation.navigateToEditarCategorias
-import com.example.gazege.ui.navigation.navigateToOneBudgetDetail
-import com.example.gazege.ui.navigation.navigateToPersonDetail
-import com.example.gazege.ui.navigation.navigateToSaldoActualSettings
-import com.example.gazege.ui.navigation.navigateToSettings
-import com.example.gazege.ui.navigation.navigateUpOrClose
-import com.example.gazege.ui.navigation.screenAccountDetail
-import com.example.gazege.ui.navigation.screenAddAccount
-import com.example.gazege.ui.navigation.screenAddCategory
-import com.example.gazege.ui.navigation.screenAddOneBudget
-import com.example.gazege.ui.navigation.screenAddPerson
-import com.example.gazege.ui.navigation.screenAddTransaction
-import com.example.gazege.ui.navigation.screenEditAccount
-import com.example.gazege.ui.navigation.screenEditBudget
-import com.example.gazege.ui.navigation.screenEditCategory
-import com.example.gazege.ui.navigation.screenEditOneBudget
-import com.example.gazege.ui.navigation.screenEditPerson
-import com.example.gazege.ui.navigation.screenEditTransaction
-import com.example.gazege.ui.navigation.screenEditarCategorias
-import com.example.gazege.ui.navigation.screenMain
-import com.example.gazege.ui.navigation.screenOneBudgetDetail
-import com.example.gazege.ui.navigation.screenPersonDetail
-import com.example.gazege.ui.navigation.screenSaldoActualSettings
-import com.example.gazege.ui.navigation.screenSettings
+import com.example.gazege.ui.navigation.MainNavHost
 import com.example.gazege.ui.theme.GazegeTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.delay
@@ -115,115 +79,12 @@ class MainActivity : ComponentActivity() {
                             }
                         })
                     {
-                        val navController = rememberNavController()
-                        NavHost(
-                            navController = navController,
-                            startDestination = "main"
-                        ) {
-                            screenMain(
-                                viewModel = mainViewModel,
-                                onNavigateToEditPerson = navController::navigateToEditPerson,
-                                onNavigateToEditTransaction = navController::navigateToEditTransaction,
-                                onNavigateToEditAccount = navController::navigateToEditAccount,
-                                onNavigateToAddPerson = navController::navigateToAddPerson,
-                                onNavigateToAddAccount = navController::navigateToAddAccount,
-                                onNavigateToSettings = navController::navigateToSettings,
-                                onNavigateToAccountDetail = navController::navigateToAccountDetail,
-                                onNavigateToAddTransaction = navController::navigateToAddTransaction,
-                                onNavigateToPersonDetail = navController::navigateToPersonDetail,
-                                onNavigateToSaldoActualSettings = navController::navigateToSaldoActualSettings,
-                                onDataLoaded = { splashScreenState.hideAndShowContent() }
-                            )
-                            screenAddAccount(
-                                viewModel = mainViewModel,
-                                onNavigateToAddPerson = navController::navigateToAddPerson,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToSettings = navController::navigateToSettings
-                            )
-                            screenEditAccount(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToSettings = navController::navigateToSettings,
-                                onNavigateToAddPerson = navController::navigateToAddPerson
-                            )
-                            screenAddPerson(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp
-                            )
-                            screenEditPerson(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp
-                            )
-                            screenAddTransaction(
-                                viewModel = mainViewModel,
-                                onNavigateUp = { navController.navigateUpOrClose { this@MainActivity.finish() } },
-                                onNavigateToAddAccount = navController::navigateToAddAccount,
-                                onDataLoaded = { splashScreenState.hideAndShowContent() }
-                            )
-                            screenEditTransaction(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToAddAccount = navController::navigateToAddAccount
-                            )
-                            screenSettings(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToAddAccount = navController::navigateToAddAccount,
-                                onNavigateToAddPerson = navController::navigateToAddPerson,
-                                onNavigateToEditCategories = navController::navigateToEditarCategorias,
-                                onNavigateToEditBudget = navController::navigateToEditBudget
-                            )
-                            screenSaldoActualSettings(viewModel = mainViewModel)
-                            screenEditarCategorias(
-                                viewModel = mainViewModel,
-                                onNavigateToAddCategory = navController::navigateToAddCategory,
-                                onNavigateToEditCategory = navController::navigateToEditCategory,
-                                onNavigateToAddBudget = {
-                                    if (it != null) {
-                                        navController.navigateToAddOneBudget(it)
-                                    } else {
-                                        navController.navigateToAddOneBudget()
-                                    }
-                                }
-                            )
-                            screenAddCategory(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp
-                            )
-                            screenEditCategory(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToEditOneBudgetRequested = navController::navigateToEditOneBudget
-                            )
-                            screenAccountDetail(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToEditAccount = navController::navigateToEditAccount,
-                                onNavigateToEditTransaction = navController::navigateToEditTransaction,
-                                onNavigateToAddTransaction = navController::navigateToAddTransaction
-                            )
-                            screenPersonDetail(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp,
-                                onNavigateToEditTransaction = navController::navigateToEditTransaction,
-                                onNavigateToEditPerson = navController::navigateToEditPerson
-                            )
-                            screenEditBudget(
-                                viewModel = mainViewModel,
-                                onNavigateToOneBudgetDetail = navController::navigateToOneBudgetDetail,
-                                onNavigateToAddOneBudget = navController::navigateToAddOneBudget,
-                                onNavigateToOneBudgetEdit = navController::navigateToEditOneBudget
-                            )
-                            screenAddOneBudget(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp
-                            )
-                            screenOneBudgetDetail(viewModel = mainViewModel)
-                            screenEditOneBudget(
-                                viewModel = mainViewModel,
-                                onNavigateUp = navController::navigateUp
-                            )
-                        }
+                        MainNavHost(
+                            navController = rememberNavController(),
+                            mainViewModel = mainViewModel,
+                            onCloseApp = { this@MainActivity.finish() },
+                            onDataLoaded = { splashScreenState.hideAndShowContent() }
+                        )
                     }
                 }
             }
--- app/src/main/java/com/example/gazege/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/gazege/ui/navigation/Screens.kt
@@ -1,8 +1,129 @@
 package com.example.gazege.ui.navigation
 
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.example.gazege.MainViewModel
 
 const val URI = "https://www.example.gazege"
+
+@Composable
+fun MainNavHost(
+    navController: NavHostController,
+    mainViewModel: MainViewModel,
+    onCloseApp: () -> Unit,
+    onDataLoaded: () -> Unit
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "main"
+    ) {
+        screenMain(
+            viewModel = mainViewModel,
+            onNavigateToEditPerson = navController::navigateToEditPerson,
+            onNavigateToEditTransaction = navController::navigateToEditTransaction,
+            onNavigateToEditAccount = navController::navigateToEditAccount,
+            onNavigateToAddPerson = navController::navigateToAddPerson,
+            onNavigateToAddAccount = navController::navigateToAddAccount,
+            onNavigateToSettings = navController::navigateToSettings,
+            onNavigateToAccountDetail = navController::navigateToAccountDetail,
+            onNavigateToAddTransaction = navController::navigateToAddTransaction,
+            onNavigateToPersonDetail = navController::navigateToPersonDetail,
+            onNavigateToSaldoActualSettings = navController::navigateToSaldoActualSettings,
+            onDataLoaded = onDataLoaded
+        )
+        screenAddAccount(
+            viewModel = mainViewModel,
+            onNavigateToAddPerson = navController::navigateToAddPerson,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToSettings = navController::navigateToSettings
+        )
+        screenEditAccount(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToSettings = navController::navigateToSettings,
+            onNavigateToAddPerson = navController::navigateToAddPerson
+        )
+        screenAddPerson(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp
+        )
+        screenEditPerson(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp
+        )
+        screenAddTransaction(
+            viewModel = mainViewModel,
+            onNavigateUp = { navController.navigateUpOrClose { onCloseApp() } },
+            onNavigateToAddAccount = navController::navigateToAddAccount,
+            onDataLoaded = onDataLoaded
+        )
+        screenEditTransaction(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToAddAccount = navController::navigateToAddAccount
+        )
+        screenSettings(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToAddAccount = navController::navigateToAddAccount,
+            onNavigateToAddPerson = navController::navigateToAddPerson,
+            onNavigateToEditCategories = navController::navigateToEditarCategorias,
+            onNavigateToEditBudget = navController::navigateToEditBudget
+        )
+        screenSaldoActualSettings(viewModel = mainViewModel)
+        screenEditarCategorias(
+            viewModel = mainViewModel,
+            onNavigateToAddCategory = navController::navigateToAddCategory,
+            onNavigateToEditCategory = navController::navigateToEditCategory,
+            onNavigateToAddBudget = {
+                if (it != null) {
+                    navController.navigateToAddOneBudget(it)
+                } else {
+                    navController.navigateToAddOneBudget()
+                }
+            }
+        )
+        screenAddCategory(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp
+        )
+        screenEditCategory(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToEditOneBudgetRequested = navController::navigateToEditOneBudget
+        )
+        screenAccountDetail(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToEditAccount = navController::navigateToEditAccount,
+            onNavigateToEditTransaction = navController::navigateToEditTransaction,
+            onNavigateToAddTransaction = navController::navigateToAddTransaction
+        )
+        screenPersonDetail(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp,
+            onNavigateToEditTransaction = navController::navigateToEditTransaction,
+            onNavigateToEditPerson = navController::navigateToEditPerson
+        )
+        screenEditBudget(
+            viewModel = mainViewModel,
+            onNavigateToOneBudgetDetail = navController::navigateToOneBudgetDetail,
+            onNavigateToAddOneBudget = navController::navigateToAddOneBudget,
+            onNavigateToOneBudgetEdit = navController::navigateToEditOneBudget
+        )
+        screenAddOneBudget(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp
+        )
+        screenOneBudgetDetail(viewModel = mainViewModel)
+        screenEditOneBudget(
+            viewModel = mainViewModel,
+            onNavigateUp = navController::navigateUp
+        )
+    }
+}
 
 fun NavController.navigateUpOrClose(
     onCloseApp: () -> Unit
@@ -12,4 +133,3 @@ fun NavController.navigateUpOrClose(
         onCloseApp()
     }
 }
-
