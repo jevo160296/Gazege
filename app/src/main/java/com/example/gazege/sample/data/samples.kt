103a40fe7e7f14fd04be7b2fa3261cf401dcd1aa
Jose Miguel Millan <jevo160296@gmail.com>
2023-07-12 12:50:12 -0500

    Simplify budget sample

--- app/src/main/java/com/example/gazege/sample/data/samples.kt
+++ app/src/main/java/com/example/gazege/sample/data/samples.kt
@@ -64,69 +64,67 @@ private fun categoriesSample(
 ) {
     val today = LocalDate.now()
     val startOfMonth = today.withDayOfMonth(1)
-    databaseSample(principalPersonAccountAmount = 5) {
-        buildSample(
-            viewModel,
-            listOf(
-                Person(0, "Pedro", 0),
-                Person(1, "Hortensia"),
-                Person(2, "Pablo"),
-                Person(3, "__ESPECIAL__")
+    buildSample(
+        viewModel,
+        listOf(
+            Person(0, "Pedro", 0),
+            Person(1, "Hortensia"),
+            Person(2, "Pablo"),
+            Person(3, "__ESPECIAL__")
+        ),
+        listOf(
+            Account(0, "Pedro", 0),
+            Account(1, "Hortensia", 1),
+            Account(2, "Pablo", 2),
+            Account(3, "__INGRESO__", 3, isIncome = true),
+            Account(4, "__GASTO__", 3, isOutcome = true)
+        ),
+        listOf(
+            Category(0, "Ingreso", null),
+            Category(1, "Hogar", null),
+            Category(2, "Renta", 1),
+            Category(3, "Servicios", null),
+            Category(4, "Luz", 3),
+            Category(5, "Agua", 3)
+        ),
+        listOf(
+            Budget.fromMonthly(0, 0, 4000000.0, BudgetType.FIXED),
+            Budget.fromMonthly(1, 2, -600000.0, BudgetType.FIXED),
+            Budget.fromMonthly(2, 4, -50000.0, BudgetType.FIXED)
+        ),
+        listOf(
+            Transaction(
+                0,
+                2500000.0,
+                sourceId = 3,
+                destinationId = 0,
+                categoryId = 0,
+                date = startOfMonth,
+                aNombreDe = null,
+                description = ""
             ),
-            listOf(
-                Account(0, "Pedro", 0),
-                Account(1, "Hortensia", 1),
-                Account(2, "Pablo", 2),
-                Account(3, "__INGRESO__", 3, isIncome = true),
-                Account(4, "__GASTO__", 3, isOutcome = true)
+            Transaction(
+                1,
+                600000.0,
+                sourceId = 0,
+                destinationId = 4,
+                categoryId = 2,
+                date = startOfMonth,
+                aNombreDe = null,
+                description = ""
             ),
-            listOf(
-                Category(0, "Ingreso", null),
-                Category(1, "Hogar", null),
-                Category(2, "Renta", 1),
-                Category(3, "Servicios", null),
-                Category(4, "Luz", 3),
-                Category(5, "Agua", 3)
-            ),
-            listOf(
-                Budget.fromMonthly(0, 0, 4000000.0, BudgetType.FIXED),
-                Budget.fromMonthly(1, 2, -600000.0, BudgetType.FIXED),
-                Budget.fromMonthly(2, 4, -50000.0, BudgetType.FIXED)
-            ),
-            listOf(
-                Transaction(
-                    0,
-                    2500000.0,
-                    sourceId = 3,
-                    destinationId = 0,
-                    categoryId = 0,
-                    date = startOfMonth,
-                    aNombreDe = null,
-                    description = ""
-                ),
-                Transaction(
-                    1,
-                    600000.0,
-                    sourceId = 0,
-                    destinationId = 4,
-                    categoryId = 2,
-                    date = startOfMonth,
-                    aNombreDe = null,
-                    description = ""
-                ),
-                Transaction(
-                    2,
-                    20000.0,
-                    sourceId = 0,
-                    destinationId = 4,
-                    categoryId = 4,
-                    date = startOfMonth,
-                    aNombreDe = null,
-                    description = ""
-                )
+            Transaction(
+                2,
+                20000.0,
+                sourceId = 0,
+                destinationId = 4,
+                categoryId = 4,
+                date = startOfMonth,
+                aNombreDe = null,
+                description = ""
             )
         )
-    }
+    )
 }
 
 private fun buildSample(
