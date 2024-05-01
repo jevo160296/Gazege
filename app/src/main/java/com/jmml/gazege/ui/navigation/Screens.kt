e64273a5dbb6f730683e6470b3d229c735584e42
Jose Miguel Millan <jevo160296@gmail.com>
2024-05-01 08:29:14 -0500

    Fix current cash settings UI issue, change navigation animations.

--- app/src/main/java/com/jmml/gazege/ui/navigation/Screens.kt
+++ app/src/main/java/com/jmml/gazege/ui/navigation/Screens.kt
@@ -55,7 +55,9 @@ fun MainNavHost(
                     } else {
                         this
                     }
-                }
+                },
+            enterTransition = { fadeIn() },
+            exitTransition = { fadeOut() }
         ) {
             screenInitialScreen(
                 viewModelInitialScreen = mainViewModel.viewModelInitial,
