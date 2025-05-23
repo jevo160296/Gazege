ccdf610c81ac7c202e8bb4e71d04b20f35d4e0d3
Jose Miguel <jmml.josemiguel@gmail.com>
2025-05-23 09:42:16 -0500

    Add initial code to develop Gazege Brío.

---
+++ brio/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.dynamic-feature")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.jmml.gazege.brio"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 29
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":app"))
+    implementation("androidx.core:core-ktx:1.16.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
