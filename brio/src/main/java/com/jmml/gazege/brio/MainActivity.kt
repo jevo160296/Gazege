2896dc861cf56a7500e36b87fad37a9c1d31e5d1
Jose Miguel <jmml.josemiguel@gmail.com>
2025-12-30 16:43:11 -0500

    Add brio main activity

---
+++ brio/src/main/java/com/jmml/gazege/brio/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.jmml.gazege.brio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+class BrioMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BrioScreen()
+        }
+    }
+}
+
+@Composable
+fun BrioScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello Brío!",
+            color = Color.Black,
+            fontSize = 24.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BrioScreenPreview() {
+    BrioScreen()
+}
